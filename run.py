5edf036c7417b637fe719bc0083274329f4124dd
VickyMutai <vicky.mutai96@gmail.com>
2018-01-20 18:19:31 +0300

    function to create a new account

--- run.py
+++ run.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3.6
+import passlocker
+def create_account(account_name, user_name, password):
+    '''
+    function to create new account
+    '''
+    new_account = credentials(account_name, user_name, password)
+    return new_account
